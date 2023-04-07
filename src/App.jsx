046542d2a64bd6d4262f2650c2ef7e66e27fea52
grandony <trinchvr@gmail.com>
2023-04-07 11:03:24 -0500

    componente Derecho Civil

--- src/App.jsx
+++ src/App.jsx
@@ -2,13 +2,15 @@ import React from 'react'
 import { Encabezado } from './components/Encabezado'
 import { Slider } from './components/Slider'
 import { Cuerpo } from './components/Cuerpo'
+import { DerechoCivil } from './components/DerechoCivil'
 
 export const App = () => {
   return (
     <>
       {/* <Encabezado/> */}
       {/* <Slider/> */}
-      <Cuerpo/>
+      {/* <Cuerpo/> */}
+      <DerechoCivil/>
     </>
 
   )
---
+++ src/components/DerechoCivil.jsx
@@ -0,0 +1,42 @@
+
+export const DerechoCivil = () => {
+  return (
+    <>
+        <div className="text-center">
+            <p className=" text-marron font-semibold">OUR PRACTICE AREAS</p>
+            <p className=" font-bold text-2xl">Top-Rated Law Firm in New York</p>
+            <div className="w-1/2 mx-auto border-b pt-1"></div>
+            <p className=" py-3">With concentration in Criminal, Civil & Federal Aviation Law</p>
+            <div className="pt-5">
+                <img className="" src="https://thekronlawfirm.com/wp-content/uploads/2023/02/shutterstock_1664392150-scaled.jpg" alt="" />
+                <div className="py-5">
+                    <p className=" text-2xl font-semibold ">CRIMINAL LAW</p>
+                    <p className="">Being convicted of a crime can lead to devastating life consequences.</p>
+                    <p className="text-marron font-semibold text-sm">Practice Areas </p>
+                </div>
+                <div className="border-b"></div>
+            </div>
+            <div className="pt-5">
+                <img className="" src="https://thekronlawfirm.com/wp-content/uploads/2023/02/shutterstock_1664392150-scaled.jpg" alt="" />
+                <div className="py-5">
+                    <p className=" text-2xl font-semibold ">CRIMINAL LAW</p>
+                    <p className="">Being convicted of a crime can lead to devastating life consequences.</p>
+                    <p className="text-marron font-semibold text-sm">Practice Areas </p>
+                </div>
+                <div className="border-b"></div>
+            </div>
+            <div className="pt-5">
+                <img className="" src="https://thekronlawfirm.com/wp-content/uploads/2023/02/shutterstock_1664392150-scaled.jpg" alt="" />
+                <div className="py-5">
+                    <p className=" text-2xl font-semibold ">CRIMINAL LAW</p>
+                    <p className="">Being convicted of a crime can lead to devastating life consequences.</p>
+                    <p className="text-marron font-semibold text-sm">Practice Areas </p>
+                </div>
+                <div className="border-b"></div>
+            </div>
+        </div>
+
+    
+    </>
+  )
+}
